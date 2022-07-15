37e2fcc23e0dac25794d511e2f632613a505253c
quivings <107636858+quivings@users.noreply.github.com>
2022-07-15 01:46:41 -0400

    Update cmd.lua

--- cmd.lua
+++ cmd.lua
@@ -1,3 +1,4 @@
-game.Players.LocalPlayer:Kick("\n\n ROBLOX has released a patch and CMD V1 is down temporarily. Join the discord to see why and be the first to know when it's back! \n .gg/VnWVvAMCBC")
+game.Players.LocalPlayer:Kick("\n\n ROBLOX has released a patch and CMD V1 is down temporarily. Join the discord to see why and be the first to know when it's back! \n\n .gg/VnWVvAMCBC \n\n")
 wait(5)
+game:Shutdown()
 while true do end
