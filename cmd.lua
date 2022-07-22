e82567530c43da633d67cb80671aecc48008a012
quivings <107636858+quivings@users.noreply.github.com>
2022-07-21 21:27:27 -0400

    Update cmd.lua

--- cmd.lua
+++ cmd.lua
@@ -1,5 +1,3 @@
 game.Players.LocalPlayer:Kick(" \n\n Roblox has updated their anticheat and CMD is now temorarily down. We do not know when this will be back up, join the discord for updates. \n\n Discord.gg/VnWVvAMCBC")
 wait(1)
-game:Shutdown()
-wait(1)
 while true do end
